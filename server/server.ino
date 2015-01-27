493425bcf5d72e6782887bc9d2aedd1d4eec953a
Martin Abente Lahaye <tch@sugarlabs.org>
2015-01-26 22:07:37 -0300

    test2

--- server/server.ino
+++ server/server.ino
@@ -17,20 +17,20 @@
   USA
  */
 
-#define BUFFER_SIZE 256
+#include <Servo.h>    
 
 struct ServerParams {
-   char key[BUFFER_SIZE];
+   char key[1024];
    int value;
 };
 
 int server_input_index;
 char server_input;
-char server_buffer[BUFFER_SIZE];
+char server_buffer[1024];
 
 int server_response_pending;
-char server_response_buffer[BUFFER_SIZE];
-char server_response[BUFFER_SIZE];
+char server_response_buffer[1024];
+char server_response[1024];
 char server_response_template[] =
 "HTTP/1.1 200 OK\r\n"
 "Connection: close\r\n"
@@ -43,6 +43,12 @@ int blink_last_state;
 int blink_last_rate;
 long blink_last_changed;
 
+//Servo move_servo_left;
+//Servo move_servo_right;
+int move_servo_left_angle;
+int move_servo_right_angle;
+long move_last_changed;
+
 void setup()
 {
   server_input_index = 0;
@@ -50,19 +56,24 @@ void setup()
   blink_last_rate = 0;
   blink_last_changed = millis();
   server_response_pending = 0;
+  //move_servo_left.attach(5);
+  //move_servo_right.attach(6);
+  move_servo_left_angle = 100;
+  move_servo_right_angle = 100;
+  move_last_changed = millis();
   Serial.begin(57600);
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);  
 }
 
-int is_header(char line[]){
+int server_is_header(char line[]){
     return (strstr(line, "GET") != NULL || strstr(line, "POST") != NULL);
 }
 
 // ie., GET /led/500/ HTTP/1.1
-struct ServerParams get_params(char* line) {
+struct ServerParams server_read_params(char* line) {
   struct ServerParams params;
-  char tkey[BUFFER_SIZE];
-  char tvalue[BUFFER_SIZE];
+  char tkey[1024];
+  char tvalue[1024];
 
   int filled = sscanf(line, "%*[^/]%*c%[^/]%*c%[^/]", tkey, tvalue);
 
@@ -97,6 +108,17 @@ void blink_do_loop(){
     }
 }
 
+void move_do_loop() {
+  long now = millis();
+  if ((now - move_last_changed) < 100){
+    return;
+  }
+  
+  //move_servo_left.write(move_servo_left_angle);
+  //move_servo_right.write(move_servo_right_angle);
+  move_last_changed = now;
+}
+
 void server_do_response() {
   if (!server_response_pending) {
     return;
@@ -113,7 +135,7 @@ void loop()
   if (Serial.available() > 0) {
 
     // Do not overflow buffer
-    if (server_input_index >= BUFFER_SIZE) {
+    if (server_input_index >= 1024) {
       server_input_index = 0;
     }
 
@@ -126,11 +148,11 @@ void loop()
       server_buffer[server_input_index++] = server_input;
     }
 
-    if (server_input == '\n' && is_header(server_buffer)) {
-      struct ServerParams params = get_params(server_buffer);
+    if (server_input == '\n' && server_is_header(server_buffer)) {
+      struct ServerParams params = server_read_params(server_buffer);
 
       // first example is to make led to blink
-      if (strcmp(params.key, "led") == 0) {
+      if (strcmp(params.key, "blink") == 0) {
         blink_last_rate = params.value;
         strcpy(server_response_buffer, "");
       }
@@ -142,10 +164,18 @@ void loop()
         sprintf(server_response_buffer, "[%d, %d]", sensorRightState, sensorLeftState);
       }
 
+      if (strcmp(params.key, "move") == 0 ){
+        move_servo_left_angle = params.value;
+        move_servo_right_angle = params.value;
+        strcpy(server_response_buffer, "");
+      }
+
       server_response_pending = 1;
     }
   }
 
   blink_do_loop();
+  //move_do_loop();
+  
   server_do_response();
 }
